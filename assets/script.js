8400b38f20fdb3e1c01a845d4a11f5498cd80532
Andrew Yang <ayang14@illinois.edu>
2016-08-30 18:51:40 -0500

    Show warning to non-Chrome users

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,22 @@
 window.onload = function () {
   var curtains = document.getElementById('curtains');
-  var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-  if (!isChrome) {
-    curtains.innerHTML = '<h1>Please use Google Chrome for the best experience :&lt;</h1>';
-    return;
-  }
-
   var slashes = document.getElementById('slashes1');
   var yangmaster = document.getElementById('yangmaster1');
   var ninja = document.getElementById('ninja1');
-  slashes.id = 'slashes';
-  yangmaster.id = 'yangmaster';
-  ninja.id = 'ninja';
+
+  var isChrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+  if (!isChrome) {
+    // curtains.innerHTML = '<h1>Please use Google Chrome for the best experience :&lt;</h1>';
+    yangmaster.id = 'yangmaster-non-chrome';
+    ninja.style.display = 'none';
+
+    var nonChromeWarning = document.getElementById('non-chrome-warning');
+    nonChromeWarning.style.display = 'default';
+  } else {
+    slashes.id = 'slashes';
+    yangmaster.id = 'yangmaster';
+    ninja.id = 'ninja';
+  }
 
   curtains.style.display = 'none';
 };
