5eb363177c86cfc6f03bf12d679e03f92b0973e4
Andrew Yang <ayang14@illinois.edu>
2016-08-30 18:07:41 -0500

    Implement window.onload to wait for images to load, and curtains

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  var slashes = document.getElementById('slashes1');
+  var yangmaster = document.getElementById('yangmaster1');
+  var ninja = document.getElementById('ninja1');
+  slashes.id = 'slashes';
+  yangmaster.id = 'yangmaster';
+  ninja.id = 'ninja';
+
+  var curtains = document.getElementById('curtains');
+  curtains.style.display = 'none';
+};
